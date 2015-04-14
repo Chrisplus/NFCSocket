e0ea471a44253c8be10b6f7c89e698200dcb434c
Shiqi Jiang <chrisplus.jiang@gmail.com>
2015-04-14 19:14:24 +0800

    ADD travis

--- nfcsocket/src/main/java/com/chrisplus/nfcsocket/HCEService.java
+++ nfcsocket/src/main/java/com/chrisplus/nfcsocket/HCEService.java
@@ -7,45 +7,46 @@ import android.os.RemoteException;
 
 public class HCEService extends CustomHostApduService {
 
-	public static final String TAG = HCEService.class.getSimpleName();
-	private Messenger serverMessenger;
+    public static final String TAG = HCEService.class.getSimpleName();
 
-	@Override
-	public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
-		if (serverMessenger != null) {
-			if (Utils.isSelectAidApdu(commandApdu)) {
-				sendMessage(NfcServerSocket.MSG_SERVER_SELECT_MESSAGE,
-						commandApdu);
-			} else {
-				sendMessage(NfcServerSocket.MSG_SERVER_NORMAL_MESSAGE,
-						commandApdu);
-			}
-		}
+    private Messenger serverMessenger;
 
-		return null;
-	}
+    @Override
+    public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
+        if (serverMessenger != null) {
+            if (Utils.isSelectAidApdu(commandApdu)) {
+                sendMessage(NfcServerSocket.MSG_SERVER_SELECT_MESSAGE,
+                        commandApdu);
+            } else {
+                sendMessage(NfcServerSocket.MSG_SERVER_NORMAL_MESSAGE,
+                        commandApdu);
+            }
+        }
 
-	@Override
-	public void onDeactivated(int reason) {
+        return null;
+    }
 
-	}
+    @Override
+    public void onDeactivated(int reason) {
 
-	@Override
-	public void onRefreshListener(Messenger sMessenger) {
-		serverMessenger = sMessenger;
-	}
+    }
 
-	private void sendMessage(int what, byte[] data) {
-		Message msg = Message.obtain(null, what);
-		Bundle bundle = new Bundle();
-		bundle.putByteArray(NfcServerSocket.DATA_KEY, data);
-		msg.setData(bundle);
-		msg.replyTo = mMessenger;
-		try {
-			serverMessenger.send(msg);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void onRefreshListener(Messenger sMessenger) {
+        serverMessenger = sMessenger;
+    }
+
+    private void sendMessage(int what, byte[] data) {
+        Message msg = Message.obtain(null, what);
+        Bundle bundle = new Bundle();
+        bundle.putByteArray(NfcServerSocket.DATA_KEY, data);
+        msg.setData(bundle);
+        msg.replyTo = mMessenger;
+        try {
+            serverMessenger.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- nfcsocket/src/main/java/com/chrisplus/nfcsocket/NfcServerSocket.java
+++ nfcsocket/src/main/java/com/chrisplus/nfcsocket/NfcServerSocket.java
@@ -14,208 +14,211 @@ import android.util.Log;
 
 /**
  * This class is used to create and operate NFC server.
- * 
- * @author Shiqi Jiang
  *
+ * @author Shiqi Jiang
  */
 public class NfcServerSocket {
 
-	public static final String TAG = NfcServerSocket.class.getSimpleName();
+    public static final String TAG = NfcServerSocket.class.getSimpleName();
 
-	/**
-	 * MSG_SERVER_SELECT_MESSAGE is sent by {@link com.chrisplus.nfcsocket.HCEService} when a select
-	 * message is received.
-	 */
-	public static final int MSG_SERVER_SELECT_MESSAGE = 1;
+    /**
+     * MSG_SERVER_SELECT_MESSAGE is sent by {@link com.chrisplus.nfcsocket.HCEService} when a
+     * select
+     * message is received.
+     */
+    public static final int MSG_SERVER_SELECT_MESSAGE = 1;
 
-	/**
-	 * MSG_SERVER_NORMAL_MESSAGE is sent by {@link com.chrisplus.nfcsocket.HCEService} when a normal
-	 * message is received.
-	 */
-	public static final int MSG_SERVER_NORMAL_MESSAGE = 2;
+    /**
+     * MSG_SERVER_NORMAL_MESSAGE is sent by {@link com.chrisplus.nfcsocket.HCEService} when a
+     * normal
+     * message is received.
+     */
+    public static final int MSG_SERVER_NORMAL_MESSAGE = 2;
 
-	/**
-	 * MSG_SERVER_DEACTIVE is send by {@link com.chrisplus.nfcsocket.HCEService} when deactivate. Two
-	 * possible scenarios making it happened: <li>The NFC link has been
-	 * deactivated or lost <li>A different AID has been selected and was
-	 * resolved to a different service component
-	 */
-	public static final int MSG_SERVER_DEACTIVE = 3;
+    /**
+     * MSG_SERVER_DEACTIVE is send by {@link com.chrisplus.nfcsocket.HCEService} when deactivate.
+     * Two
+     * possible scenarios making it happened: <li>The NFC link has been
+     * deactivated or lost <li>A different AID has been selected and was
+     * resolved to a different service component
+     */
+    public static final int MSG_SERVER_DEACTIVE = 3;
 
-	/**
-	 * The key of response data in bundle.
-	 */
-	public static final String DATA_KEY = "key";
+    /**
+     * The key of response data in bundle.
+     */
+    public static final String DATA_KEY = "key";
 
-	private static NfcServerSocket instance;
-	private NfcServerSocketListener listener;
-	private Context context;
-	private Messenger coreNfcMessenger;
-	private final Messenger localMessenger = new Messenger(new MsgHandler());
-	private Intent intent;
-	private final ServiceConnection serviceConnection = new ServiceConnection() {
+    private static NfcServerSocket instance;
 
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			log("hce service connected & send localMessenger");
-			coreNfcMessenger = new Messenger(service);
-			Message msg = Message.obtain(null, HCEService.MSG_REFRESH_SERVER);
-			msg.replyTo = localMessenger;
-			try {
-				coreNfcMessenger.send(msg);
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
-		}
+    private NfcServerSocketListener listener;
 
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			log("hce service disconnected");
-			if (coreNfcMessenger != null) {
-				log("send null messenger");
-				Message msg = Message.obtain(null,
-						HCEService.MSG_REFRESH_SERVER);
-				msg.replyTo = null;
-				try {
-					coreNfcMessenger.send(msg);
-				} catch (RemoteException e) {
-					e.printStackTrace();
-				}
-			}
+    private Context context;
 
-		}
-	};
+    private Messenger coreNfcMessenger;
 
-	/**
-	 * Get an instance of NfcServerSocket
-	 * 
-	 * @param context
-	 *            The application context.
-	 * @return The instance of NfcServerSocket
-	 */
-	public static NfcServerSocket getInstance(Context context) {
-		if (instance == null) {
-			instance = new NfcServerSocket(context);
-		}
-		return instance;
-	}
+    private final Messenger localMessenger = new Messenger(new MsgHandler());
 
-	private NfcServerSocket(Context ctx) {
-		context = ctx;
-		intent = new Intent(context, HCEService.class);
-	}
+    private Intent intent;
 
-	/**
-	 * Start the Nfc service and listening the coming reader message.
-	 */
-	public void listen() {
-		if (listener != null) {
-			log("start listen");
-			context.bindService(intent, serviceConnection,
-					Context.BIND_AUTO_CREATE);
-			context.startService(intent);
-		}
-	}
+    private final ServiceConnection serviceConnection = new ServiceConnection() {
 
-	/**
-	 * Stop the Nfc service.
-	 */
-	public void close() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            log("hce service connected & send localMessenger");
+            coreNfcMessenger = new Messenger(service);
+            Message msg = Message.obtain(null, HCEService.MSG_REFRESH_SERVER);
+            msg.replyTo = localMessenger;
+            try {
+                coreNfcMessenger.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
 
-		context.unbindService(serviceConnection);
-		if (context.stopService(intent)) {
-			log("stop nfc service");
-		}
-	}
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            log("hce service disconnected");
+            if (coreNfcMessenger != null) {
+                log("send null messenger");
+                Message msg = Message.obtain(null,
+                        HCEService.MSG_REFRESH_SERVER);
+                msg.replyTo = null;
+                try {
+                    coreNfcMessenger.send(msg);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
 
-	/**
-	 * Set a listener which can get callback when the Nfc server gets messages.
-	 * 
-	 * @param lst
-	 *            A {NfcServerSocketListener} used to get the callback when
-	 *            messages come.
-	 */
-	public void setListener(NfcServerSocketListener lst) {
-		if (lst != null) {
-			log("set listener");
-			listener = lst;
-		}
-	}
+        }
+    };
 
-	/**
-	 * The interface used to get callback when message comes.
-	 * 
-	 * @author Shiqi Jiang
-	 *
-	 */
-	public interface NfcServerSocketListener {
+    /**
+     * Get an instance of NfcServerSocket
+     *
+     * @param context The application context.
+     * @return The instance of NfcServerSocket
+     */
+    public static NfcServerSocket getInstance(Context context) {
+        if (instance == null) {
+            instance = new NfcServerSocket(context);
+        }
+        return instance;
+    }
 
-		/**
-		 * The callback when the select command is received. The connection will
-		 * NOT be created if null is returned.
-		 * 
-		 * @param message
-		 *            The select message containing AID generally.
-		 * @return You should not return null if you confirm to create a
-		 *         connection with client side.
-		 */
-		public byte[] onSelectMessage(byte[] message);
+    private NfcServerSocket(Context ctx) {
+        context = ctx;
+        intent = new Intent(context, HCEService.class);
+    }
 
-		/**
-		 * The callback when normal command (message) is received.
-		 * 
-		 * @param message
-		 *            The normal message.
-		 * @return The response message. It could be null.
-		 */
-		public byte[] onMessage(byte[] message);
+    /**
+     * Start the Nfc service and listening the coming reader message.
+     */
+    public void listen() {
+        if (listener != null) {
+            log("start listen");
+            context.bindService(intent, serviceConnection,
+                    Context.BIND_AUTO_CREATE);
+            context.startService(intent);
+        }
+    }
 
-	}
+    /**
+     * Stop the Nfc service.
+     */
+    public void close() {
 
-	private class MsgHandler extends Handler {
+        context.unbindService(serviceConnection);
+        if (context.stopService(intent)) {
+            log("stop nfc service");
+        }
+    }
 
-		@Override
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case MSG_SERVER_SELECT_MESSAGE:
-				if (listener != null) {
-					byte[] response = listener.onSelectMessage(msg.getData()
-							.getByteArray(DATA_KEY));
-					sendResponse(response);
-				}
-				break;
-			case MSG_SERVER_NORMAL_MESSAGE:
-				if (listener != null) {
-					byte[] response = listener.onMessage(msg.getData()
-							.getByteArray(DATA_KEY));
-					sendResponse(response);
-				}
-				break;
-			default:
-				super.handleMessage(msg);
-			}
+    /**
+     * Set a listener which can get callback when the Nfc server gets messages.
+     *
+     * @param lst A {NfcServerSocketListener} used to get the callback when
+     *            messages come.
+     */
+    public void setListener(NfcServerSocketListener lst) {
+        if (lst != null) {
+            log("set listener");
+            listener = lst;
+        }
+    }
 
-		}
-	}
+    /**
+     * The interface used to get callback when message comes.
+     *
+     * @author Shiqi Jiang
+     */
+    public interface NfcServerSocketListener {
 
-	private void sendResponse(byte[] response) {
-		if (coreNfcMessenger != null) {
-			Message message = Message
-					.obtain(null, HCEService.MSG_RESPONSE_APDU);
-			Bundle dataBundle = new Bundle();
-			dataBundle.putByteArray(HCEService.KEY_DATA, response);
-			message.setData(dataBundle);
-			try {
-				coreNfcMessenger.send(message);
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
-		}
+        /**
+         * The callback when the select command is received. The connection will
+         * NOT be created if null is returned.
+         *
+         * @param message The select message containing AID generally.
+         * @return You should not return null if you confirm to create a
+         * connection with client side.
+         */
+        public byte[] onSelectMessage(byte[] message);
 
-	}
+        /**
+         * The callback when normal command (message) is received.
+         *
+         * @param message The normal message.
+         * @return The response message. It could be null.
+         */
+        public byte[] onMessage(byte[] message);
 
-	private void log(String logMessage) {
-		Log.d(TAG, logMessage);
-	}
+    }
+
+    private class MsgHandler extends Handler {
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SERVER_SELECT_MESSAGE:
+                    if (listener != null) {
+                        byte[] response = listener.onSelectMessage(msg.getData()
+                                .getByteArray(DATA_KEY));
+                        sendResponse(response);
+                    }
+                    break;
+                case MSG_SERVER_NORMAL_MESSAGE:
+                    if (listener != null) {
+                        byte[] response = listener.onMessage(msg.getData()
+                                .getByteArray(DATA_KEY));
+                        sendResponse(response);
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+
+        }
+    }
+
+    private void sendResponse(byte[] response) {
+        if (coreNfcMessenger != null) {
+            Message message = Message
+                    .obtain(null, HCEService.MSG_RESPONSE_APDU);
+            Bundle dataBundle = new Bundle();
+            dataBundle.putByteArray(HCEService.KEY_DATA, response);
+            message.setData(dataBundle);
+            try {
+                coreNfcMessenger.send(message);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void log(String logMessage) {
+        Log.d(TAG, logMessage);
+    }
 
 }
